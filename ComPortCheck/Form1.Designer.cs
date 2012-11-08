b49232c26cb11c852b8cfe809b6486a6f5448cf2
YUJI KAMINO <yujikamino@gmail.com>
2012-11-09 08:22:51 +0900

    アップ開始

---
+++ ComPortCheck/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ComPortCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testStartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Judgelabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // testStartButton
+            // 
+            this.testStartButton.Location = new System.Drawing.Point(31, 207);
+            this.testStartButton.Name = "testStartButton";
+            this.testStartButton.Size = new System.Drawing.Size(75, 23);
+            this.testStartButton.TabIndex = 0;
+            this.testStartButton.Text = "開始";
+            this.testStartButton.UseVisualStyleBackColor = true;
+            this.testStartButton.Click += new System.EventHandler(this.testStartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TRX1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(181, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "COMポート";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "TRX2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "EXT1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "EXT2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(181, 212);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "判定：";
+            // 
+            // Judgelabel
+            // 
+            this.Judgelabel.AutoSize = true;
+            this.Judgelabel.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Judgelabel.Location = new System.Drawing.Point(231, 206);
+            this.Judgelabel.Name = "Judgelabel";
+            this.Judgelabel.Size = new System.Drawing.Size(22, 24);
+            this.Judgelabel.TabIndex = 16;
+            this.Judgelabel.Text = "-";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(151, 81);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(151, 120);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 20);
+            this.comboBox3.TabIndex = 19;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(151, 158);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 20);
+            this.comboBox4.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 258);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Judgelabel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.testStartButton);
+            this.Name = "Form1";
+            this.Text = "ComPortCheck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testStartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label Judgelabel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+    }
+}
+
---
+++ ComPortCheck/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+	
+namespace ComPortCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            string[] PortList = SerialPort.GetPortNames();
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            comboBox4.Items.Clear();
+            foreach (string PortName in PortList)
+            {
+                comboBox1.Items.Add(PortName);
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+            foreach (string PortName in PortList)
+            {
+                comboBox2.Items.Add(PortName);
+            }
+            if (comboBox2.Items.Count > 0)
+            {
+                comboBox2.SelectedIndex = 0;
+            }
+            foreach (string PortName in PortList)
+            {
+                comboBox3.Items.Add(PortName);
+            }
+            if (comboBox3.Items.Count > 0)
+            {
+                comboBox3.SelectedIndex = 0;
+            }
+            foreach (string PortName in PortList)
+            {
+                comboBox4.Items.Add(PortName);
+            }
+            if (comboBox4.Items.Count > 0)
+            {
+                comboBox4.SelectedIndex = 0;
+            }
+        }
+
+
+        private void testStartButton_Click(object sender, EventArgs e)
+        {
+            int BaudRate = 9600;
+            Parity Parity = Parity.None;
+            int DataBits = 8;
+            StopBits StopBits = StopBits.One;
+            SerialPort myPort =
+            new SerialPort(comboBox1.Text, BaudRate, Parity, DataBits, StopBits);
+            SerialPort myPort2 =
+            new SerialPort(comboBox2.Text, BaudRate, Parity, DataBits, StopBits);
+            SerialPort myPort3 =
+            new SerialPort(comboBox3.Text, BaudRate, Parity, DataBits, StopBits);
+            SerialPort myPort4 =
+            new SerialPort(comboBox4.Text, BaudRate, Parity, DataBits, StopBits);
+
+            try
+            {
+                myPort.Open();
+                myPort2.Open();
+                myPort3.Open();
+                myPort4.Open();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+            //TRX1->TRX2 送信
+            int result = 0;
+            do
+            {
+                if (!dataSendTest(myPort, myPort2))
+                {
+                    break;
+                }
+                if (!dataSendTest(myPort2, myPort))
+                {
+                    break;
+                }
+                if (!dataSendTest(myPort3, myPort4))
+                {
+                    break;
+                }
+                if (!dataSendTest(myPort4, myPort3))
+                {
+
+                    break;
+                }
+                result = 1;
+            } while (false);
+
+            if (result == 1)
+                Judgelabel.Text = "OK";
+            else
+                Judgelabel.Text = "NG";
+
+            portCloseall(myPort,myPort2,myPort3,myPort4);
+
+        }
+
+        private Boolean dataSendTest(SerialPort myPort, SerialPort myPort2)
+        {
+            if (myPort.IsOpen == false)
+            {
+                return false;
+            }
+            try
+            {
+                myPort.WriteLine("ABCDEF");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            string data = null;
+            System.Threading.Thread.Sleep(100);
+            try
+            {
+                data = myPort2.ReadExisting();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //string data = myPort2.ReadLine();
+
+            int iCompare = data.CompareTo("ABCDEF\n");
+            if (iCompare == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        private void portCloseall(SerialPort myPort, SerialPort myPort2, SerialPort myPort3, SerialPort myPort4)
+        {
+            myPort.Close();
+            myPort.Dispose();
+            myPort2.Close();
+            myPort2.Dispose();
+            myPort3.Close();
+            myPort3.Dispose();
+            myPort4.Close();
+            myPort4.Dispose();
+        }         
+    }
+}
